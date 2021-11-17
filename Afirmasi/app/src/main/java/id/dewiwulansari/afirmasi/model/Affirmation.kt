7665657c77d7b7d23508f67ebbdb1ee802d7ec0e
Dewi Wulansari <195410031@students.akakom.ac.id>
2021-11-17 16:16:30 +0700

    Afirmasi

---
+++ Afirmasi/app/src/main/java/id/dewiwulansari/afirmasi/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package id.dewiwulansari.afirmasi.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation
+    (@StringRes val stringResourceId: Int,
+     @DrawableRes val imageResourceId: Int ){
+}
